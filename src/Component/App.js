ea960865d76238339724eaeee31bd89b61ebdb37
Mr-BK <bogharakuldip@gmail.com>
2022-02-09 11:20:01 -0500

    first commit

---
+++ src/Component/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import '../CSS/grid.css';
+
+const App = () => {
+  return (
+    <diV id="frame">
+      <div id="avtr"> AVTR</div>
+      <div id="contact"> Contact</div>
+    </diV>
+  );
+};
+
+export default App;
